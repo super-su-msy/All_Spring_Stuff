71866d719d90c9a2a4762250378a62bfdd14e2db
Karan <karan.m@gmx.com>
2020-04-03 22:24:53 -0400

    Succesfully implemented EhCache w/ Jsr-107

--- spring-ehcache/src/main/java/com/baeldung/cachetest/rest/NumberController.java
+++ spring-ehcache/src/main/java/com/baeldung/cachetest/rest/NumberController.java
@@ -1,5 +1,8 @@
 package com.baeldung.cachetest.rest;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +18,25 @@ import com.baeldung.cachetest.service.NumberService;
 @RequestMapping(path = "/number", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class NumberController {
 
-    private final static Logger log = LoggerFactory.getLogger(NumberController.class);
+	private final static Logger log = LoggerFactory.getLogger(NumberController.class);
 
-    @Autowired
-    private NumberService numberService;
+	@Autowired
+	private NumberService numberService;
 
-    @GetMapping(path = "/square/{number}")
-    public String getThing(@PathVariable Long number) {
-        log.info("call numberService to square {}", number);
-        return String.format("{\"square\": %s}", numberService.square(number));
-    }
+	@GetMapping(path = "/square/{number}")
+	public String getThing(@PathVariable Long number) {
+		log.info("call numberService to square {}", number);
+		return String.format("{\"square\": %s}", numberService.square(number));
+	}
+
+	@GetMapping(path = "/comments")
+	public String hellYeah() {
+		Instant start = Instant.now();
+		String s = numberService.hellYeah();
+		Instant finish = Instant.now();
+		long timeElapsed = Duration.between(start, finish).toMillis(); // in millis
+		log.info("time  numberService to square {}", timeElapsed);
+		return s;
+	}
 
 }
--- spring-ehcache/src/main/java/com/baeldung/cachetest/service/NumberService.java
+++ spring-ehcache/src/main/java/com/baeldung/cachetest/service/NumberService.java
@@ -5,19 +5,40 @@ import java.math.BigDecimal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class NumberService {
 
-    private final static Logger log = LoggerFactory.getLogger(NumberService.class);
+	private final static Logger log = LoggerFactory.getLogger(NumberService.class);
 
-    @Cacheable(value = "squareCache", key = "#number", condition = "#number>10")
-    public BigDecimal square(Long number) {
-        BigDecimal square = BigDecimal.valueOf(number)
-            .multiply(BigDecimal.valueOf(number));
-        log.info("square of {} is {}", number, square);
-        return square;
-    }
+	@Cacheable(value = "squareCache", key = "#number", condition = "#number>10")
+	public BigDecimal square(Long number) {
+		BigDecimal square = BigDecimal.valueOf(number).multiply(BigDecimal.valueOf(number));
+		log.info("square of {} is {}", number, square);
+		return square;
+	}
+
+	@Cacheable(value = "photos")
+	public String hellYeah() {
+		// TODO Auto-generated method stub
+		log.info("Check this bro **********************************");
+		RestTemplate restTemplate = new RestTemplate();
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+
+		HttpEntity<?> entity = new HttpEntity<>(headers);
+
+		HttpEntity<String> response = restTemplate.exchange("https://jsonplaceholder.typicode.com/photos",
+				HttpMethod.GET, entity, String.class);
+		return response.getBody().toString();
+
+	}
 
 }
